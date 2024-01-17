e96b9feb13e82b6a8a75d90ab0e6a0eeec556c48
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-17 09:39:09 -0700

    finished evenNumbers

--- evenNumbers.js
+++ evenNumbers.js
@@ -1,10 +1,10 @@
-// your code here
-
-
-
-
-
-
+let evenNumbers = function(max) {
+  let array = [];
+  for (let i = 2; i < max; i += 2) {
+    array.push(i);
+  }
+  return array;
+};
 
 console.log(evenNumbers(7)); // [ 2, 4, 6 ]
 console.log(evenNumbers(12)); // [ 2, 4, 6, 8, 10 ]
