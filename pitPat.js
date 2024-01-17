228c247c7e2754f28cf71c18b7ca4845b8c100d2
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-17 11:11:25 -0700

    finished pitPat

--- pitPat.js
+++ pitPat.js
@@ -1,10 +1,14 @@
-// your code here
-
-
-
-
-
-
+function pitPat(max) {
+  let array = [];
+  for (let i = 0; i <= max; i++) {
+    if (i % 4 === 0 && i % 6 !== 0) {
+      array.push(i);
+    } else if (i % 6 === 0 && i % 4 !== 0) {
+      array.push(i);
+    }
+  }
+  return array;
+};
 
 console.log(pitPat(18)); // [ 4, 6, 8, 16, 18 ]
 console.log(pitPat(30)); // [ 4, 6, 8, 16, 18, 20, 28, 30 ]
