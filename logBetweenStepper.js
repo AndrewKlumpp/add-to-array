f4205e9e72d9367271a255c4fc384777325bb3ef
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-17 09:55:51 -0700

    finished logBetweenStepper

--- logBetweenStepper.js
+++ logBetweenStepper.js
@@ -1,7 +1,11 @@
-
+let logBetweenStepper = function(min, max, step) {
+  for (let i = min; i <= max; i += step) {
+    console.log(i);
+  }
+};
 
 // Examples:
-// logBetweenStepper(5, 9, 1); // prints out:
+logBetweenStepper(5, 9, 1); // prints out:
 // 5
 // 6
 // 7
@@ -9,7 +13,7 @@
 // 9
 
 
-// logBetweenStepper(-10, 15, 5)  // prints out:
+logBetweenStepper(-10, 15, 5)  // prints out:
 // -10
 // -5
 // 0
