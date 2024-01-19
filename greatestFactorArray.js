fd25b5c467e5662acd1799dd6d6227f65b7c9538
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-19 12:26:51 -0700

    finished greatestFactorArray

--- greatestFactorArray.js
+++ greatestFactorArray.js
@@ -1,6 +1,22 @@
 function greatestFactorArray(array) {
-  // your code here
-}
+  let greatest = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      greatest.push(greatestFactor(array[i]))
+    } else {
+      greatest.push(array[i])
+      }
+    }
+  return greatest;
+};
+
+function greatestFactor(num) {
+  for (let i = num -1; i > 0; i--) {
+    if (num % i === 0) {
+      return i;
+    }
+  }
+};
 
 console.log(greatestFactorArray([16, 7, 9, 14])); // [8, 7, 9, 7]
 console.log(greatestFactorArray([30, 3, 24, 21, 10])); // [15, 3, 12, 21, 5]
