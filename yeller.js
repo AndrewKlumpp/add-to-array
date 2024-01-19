56bf5009a000a50d602f47c636bcf6dac9aa9af8
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-19 09:27:12 -0700

    finished yeller

--- yeller.js
+++ yeller.js
@@ -1,6 +1,12 @@
 function yeller(words) {
-
-}
+  let yelled = [];
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let yelledWord = word.toUpperCase() + "!";
+    yelled.push(yelledWord);
+  }
+  return yelled;
+};
 
 
 console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
