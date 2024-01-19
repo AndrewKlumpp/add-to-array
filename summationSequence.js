b06a36ebf3f7fe415f37d0e96694e70281feed9a
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-19 12:54:21 -0700

    finished summationSequence

--- summationSequence.js
+++ summationSequence.js
@@ -1,6 +1,18 @@
 function summationSequence(start, length) {
-  // your code here
-}
+  let seq = [start];
+  for (let i = 1; i < length; i++) {
+    seq.push(summation(seq[i - 1]));
+  }
+  return seq;
+};
+
+function summation(num) {
+  let total = 0;
+  for (let i = 1; i <= num; i++) {
+    total = total + i;
+  }
+  return total;
+};
 
 console.log(summationSequence(3, 4)); // [3, 6, 21, 231]
 console.log(summationSequence(5, 3)); // [5, 15, 120]
