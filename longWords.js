3a3735769ad70cc9f120a1ee7edd059a4a900d4d
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-19 09:58:38 -0700

    initial commit

---
+++ longWords.js
@@ -0,0 +1,8 @@
+function longWords(words) {
+
+}
+
+
+
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
