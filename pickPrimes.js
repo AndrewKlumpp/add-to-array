87835d54a87caadce2e9429e04d8cd984407b533
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-19 11:58:43 -0700

    initial commit

---
+++ pickPrimes.js
@@ -0,0 +1,8 @@
+function pickPrimes(array) {
+  // your code here
+}
+
+
+
+console.log(pickPrimes([2, 3, 4, 5, 6]));  // [2, 3, 5]
+console.log(pickPrimes([101, 20, 103, 2017]));  // [101, 103, 2017]
