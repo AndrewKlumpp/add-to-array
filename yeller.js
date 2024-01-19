50980fe940b153109307902c9663d20552f96f85
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-19 09:20:48 -0700

    initial commit

---
+++ yeller.js
@@ -0,0 +1,7 @@
+function yeller(words) {
+
+}
+
+
+console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
+console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANGO!' ]
